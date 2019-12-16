e0d6dfc0fd2aa54242e686811b3bd22a3793747c
Georg Jung <git@gjung.com>
2019-12-16 19:50:49 +0200

    Add syntax extensions for some syntactic sugar

---
+++ src/FluentCloudflare/Extensions/SyntaxExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentCloudflare.Abstractions.Builders.Dns;
+using FluentCloudflare.Abstractions.Infrastructure;
+using FluentCloudflare.Api;
+using FluentCloudflare.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentCloudflare.Extensions
+{
+    public static class SyntaxExtensions
+    {
+        public static IDnsUpdateSyntax Update(this IDnsSyntax syntax, DnsRecord record, DnsRecordType type, string name, string content)
+            => syntax.Update(record.Id, type, name, content);
+
+        public static IApiMethod<EntryReference> Delete(this IDnsSyntax syntax, DnsRecord record)
+            => syntax.Delete(record.Id);
+    }
+}
