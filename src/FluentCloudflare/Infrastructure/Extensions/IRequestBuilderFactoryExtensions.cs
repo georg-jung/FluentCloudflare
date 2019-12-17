a7fed6bab26908a5343ea201fab4e175e974e189
Georg Jung <git@gjung.com>
2019-12-17 22:01:45 +0200

    Add more CreateApiMethod extensions

--- src/FluentCloudflare/Infrastructure/Extensions/IRequestBuilderFactoryExtensions.cs
+++ src/FluentCloudflare/Infrastructure/Extensions/IRequestBuilderFactoryExtensions.cs
@@ -12,7 +12,27 @@ namespace FluentCloudflare.Infrastructure.Extensions
         public static ResponseApiMethodBuilder<TEntity> CreateApiMethod<TEntity>(
             this IRequestBuilderFactory factory,
             HttpMethod method,
-            ExpandoObject queryStringParameters = null)
-        => ResponseApiMethodBuilder<TEntity>.Create(factory, method, queryStringParameters);
+            ExpandoObject queryStringParameters = null,
+            ExpandoObject body = null)
+        => ResponseApiMethodBuilder<TEntity>.Create(factory, method, queryStringParameters, body);
+        
+        public static ResponseApiMethodBuilder<TEntity> CreateApiMethod<TEntity>(
+            this IRequestBuilderFactory factory,
+            ExpandoObject queryStringParameters = null,
+            ExpandoObject body = null)
+        => ResponseApiMethodBuilder<TEntity>.Create(factory, queryStringParameters, body);
+
+        public static ApiMethodBuilder CreateApiMethod(
+            this IRequestBuilderFactory factory,
+            HttpMethod method,
+            ExpandoObject queryStringParameters = null,
+            ExpandoObject body = null)
+            => ApiMethodBuilder.Create(factory, method, queryStringParameters, body);
+
+        public static ApiMethodBuilder CreateApiMethod(
+            this IRequestBuilderFactory factory,
+            ExpandoObject queryStringParameters = null,
+            ExpandoObject body = null)
+            => ApiMethodBuilder.Create(factory, queryStringParameters, body);
     }
 }
