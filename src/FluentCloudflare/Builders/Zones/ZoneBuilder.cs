68b457c2b7c4f28d86b7695d0bc7515e5eba7ded
Georg Jung <git@gjung.com>
2020-01-07 18:59:18 +0200

    Refactor ZoneBuilder

---
+++ src/FluentCloudflare/Builders/Zones/ZoneBuilder.cs
@@ -0,0 +1,20 @@
+﻿using FluentCloudflare.Abstractions.Builders;
+using FluentCloudflare.Abstractions.Builders.Dns;
+using FluentCloudflare.Abstractions.Builders.Zones;
+using FluentCloudflare.Abstractions.Infrastructure;
+using FluentCloudflare.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentCloudflare.Builders.Zones
+{
+    internal class ZoneBuilder : UrlExtender, IZoneSyntax
+    {
+        internal ZoneBuilder(IRequestBuilderFactory context, string identifier) : base(context, "zones", identifier)
+        {
+        }
+
+        public IDnsSyntax Dns => new Dns.DnsBuilder(this);
+    }
+}
