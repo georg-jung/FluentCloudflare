a060677874a6a27b7a6e4c49dd21e1aa4610eefa
Georg Jung <git@gjung.com>
2020-01-07 20:48:13 +0200

    suppress warning regarding naming of Error class

--- src/FluentCloudflare/Api/Error.cs
+++ src/FluentCloudflare/Api/Error.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace FluentCloudflare.Api
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1716:Bezeichner dürfen nicht mit Schlüsselwörtern übereinstimmen", Justification = "This is called error by the API. Consumers possibly don't instantiate this class and in CSharp and FSharp this isn't a keyword. Conciseness over strange VB use cases.")]
     public class Error
     {
         public int Code { get; set; }
