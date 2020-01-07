a45ebc6eb222bd9dfbe5f4fa8a903e985028fdac
Georg Jung <git@gjung.com>
2020-01-07 19:31:18 +0200

    Refactor suppress warnings for one member near it's definition

--- src/FluentCloudflare/Api/Response.cs
+++ src/FluentCloudflare/Api/Response.cs
@@ -11,12 +11,16 @@ namespace FluentCloudflare.Api
     {
         public TResult Result { get; set; }
         public bool Success { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Sammlungseigenschaften müssen schreibgeschützt sein", Justification = "This represents poco-style entities returned by the rest API which are deserialized.")]
         public List<Error> Errors { get; set; }
+
         // Messages seems to be unused by the Cloudflare API, so it is not implemented here.
         // It does not seem to make sense to assume any details what a message could be, so
         // regarding backwards compatibility of future releases it seems preferable to leave
         // this unimplemented.
         // Messages
+        
         public ResultInfo ResultInfo { get; set; }
         public HttpStatusCode StatusCode { get; set; }
 
